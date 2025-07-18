81118eed63c614cdaceb684b978fb92cb05303ef
ferxxu <federicocoronado2006@gmail.com>
2025-07-18 08:20:07 -0300

    Recuperando versión anterior

--- whiteagleAgency/src/App.jsx
+++ whiteagleAgency/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from './pages/layout/layout'
 import Home from "./pages/home/home"
 import Staff from "./pages/staff"
 import Services from "./pages/services"
-import Courses from "./pages/courses"
+import Courses from "./pages/courses/courses"
 import NotFound from "./pages/noPage"
 
 export default function App() {
