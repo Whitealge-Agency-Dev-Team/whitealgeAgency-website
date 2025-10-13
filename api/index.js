073625acb5b560692cb8de798e1026691928892c
ferxxu <federicocoronado2006@gmail.com>
2025-10-13 12:28:21 -0300

    uploading middlewares [part 1]

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const sequelize = require("./config/database");
 const cors = require("cors");
 const PORT = 3000;
 const server = express();
+const { authlimiter, apiLimiter } = require('./middlewares/rateLimit/rateLimiting');
 
 server.use(express.json());
 
@@ -15,6 +16,11 @@ server.use(
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
+server.use('/register', authlimiter);
+server.use('/login', authlimiter);
+server.use('/', apiLimiter);
+
+
 
 sequelize
   .sync({ force: false })
